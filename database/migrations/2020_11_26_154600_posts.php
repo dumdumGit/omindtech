7ff2d3cd7c738b194e67bd6db2f84a10519c8ae3
Sadan <urang.sadan@gmail.com>
2020-11-27 14:39:49 +0700

    increment id

--- database/migrations/2020_11_26_154600_posts.php
+++ database/migrations/2020_11_26_154600_posts.php
@@ -14,7 +14,7 @@ class Posts extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('title', 100);
             $table->longText('content');
             $table->smallInteger('published')->nullable()->default(0);
--- database/migrations/2020_11_27_022922_create_galleries_table.php
+++ database/migrations/2020_11_27_022922_create_galleries_table.php
@@ -14,7 +14,7 @@ class CreateGalleriesTable extends Migration
     public function up()
     {
         Schema::create('galleries', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('title', 100);
             $table->string('image_url');
             $table->text('description')->nullable();
